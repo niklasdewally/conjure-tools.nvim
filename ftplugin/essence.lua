07717ac8d62dcd0d303ecd0e3f62820405f3acf8
Niklas Dewally <niklas@dewally.com>
2023-07-23 13:40:07 +0100

    WIP: Fix directory layout for vscode snippets

--- ftplugin/essence.lua
+++ ftplugin/essence.lua
@@ -10,6 +10,6 @@ local ok,vscode_snipper_loader = pcall(require,"luasnip.loaders.from_vscode")
 if ok then
   local dirname = vim.fs.dirname
   local package_dir = dirname(dirname(debug.getinfo(1,'S').source))
-  local snippet_path = package_dir .. "/snippets"
+  local snippet_path = package_dir .. "/vscode-snippets"
   vscode_snipper_loader.load({paths = snippet_path})
 end
